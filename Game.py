241a6a0e60846f5424f26b2c7745195c5861f426
DESKTOP-H77RVV6\ycy12 <ycy1208@gmail.com>
2022-04-27 15:57:03 +0100

    End of lesson working on MVP

--- Game.py
+++ Game.py
@@ -1,18 +1,27 @@
 
 class Game:
+    EMPTY = " "
+    P1 = "o"
+    P2 = "x"
 
     def __init__(self):
-        pass
+        self.__board = [[Game.EMPTY for _ in range(3)] for _ in range(3)]
+        self.__player = Game.p1
 
     def __repr__(self):
-        pass
+        return "This is the board"
 
     def play(self,row,col):
-        pass
-    
+        row -= 1 # to zero base
+        col -= 1
+        self.board[row][col] = self.__player
+        self.__player = Game.P2 if self.__player == Game.P1 else Game.P1
+        
+
     @property
     def winner(self):
-        pass
+        return None
+        
 
 if __name__ == "__main__":
     pass
